50662f9eea2d15a20f64327825efd80a90aa4ef4
Alejandro <delavega@colorado.edu>
2017-01-09 17:59:53 -0600

    For deepdream

--- extract_forrest.py
+++ extract_forrest.py
@@ -3,13 +3,14 @@ import pandas as pd
 from pliers.stimuli.video import VideoStim
 from pliers.converters.video import FrameSamplingConverter
 from pliers.extractors.google import GoogleVisionAPIFaceExtractor
+import os
 
-dataset_dir = '/Users/alejandro/datasets/forrest/phase2/'
+dataset_dir = '/mnt/c/Users/aid338/Documents/forrest/'
 
 video = os.join(dataset_dir, 'stimuli/movie/fg_av_seg0.mkb')
 
 # Sample video
-conv = FrameSamplingConverter(every=15)
+conv = FrameSamplingConverter(hertz=2)
 derived = conv.transform(video)
 
 ext = GoogleVisionAPIFaceExtractor(discovery_file='/Users/alejandro/bin/forrest-1c48f2c6a8c9.json')
