d47c77ec7501cf1fdf70683ae166c515e0f18f00
Alejandro <delavega@colorado.edu>
2017-01-09 18:42:16 -0600

    for Windows

--- extract_forrest.py
+++ extract_forrest.py
@@ -5,14 +5,14 @@ from pliers.converters.video import FrameSamplingConverter
 from pliers.extractors.google import GoogleVisionAPIFaceExtractor
 import os
 
-dataset_dir = '/mnt/c/Users/aid338/Documents/forrest/'
+dataset_dir = 'C:/Users/aid338/Documents/forrest/'
 
-video = os.path.join(dataset_dir, 'stimuli/movie/fg_av_seg0.mkb')
+video = os.path.join(dataset_dir, 'stimuli/movie/fg_av_seg0.mkv')
 
 # Sample video
 conv = FrameSamplingConverter(hertz=1)
 derived = conv.transform(video)
 
-ext = GoogleVisionAPIFaceExtractor(discovery_file='/mnt/c/Users/aid338/Documents/forrest-1c48f2c6a8c9.json')
+ext = GoogleVisionAPIFaceExtractor(discovery_file='C:/Users/aid338/Documents/forrest-1c48f2c6a8c9.json')
 
 features = [ext.transform(frame) for frame in derived]
