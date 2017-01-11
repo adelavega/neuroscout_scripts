cd778baa00db34451fef2ec64eb2d124828d20a3
adelavega <delavega@utexas.edu>
2017-01-11 16:33:36 -0600

    Added forrest gump vision results

--- extract_forrest.py
+++ extract_forrest.py
@@ -3,16 +3,27 @@ import pandas as pd
 from pliers.stimuli.video import VideoStim
 from pliers.converters.video import FrameSamplingConverter
 from pliers.extractors.google import GoogleVisionAPIFaceExtractor
-import os
+from os.path import join, isfile
+from glob import glob
+import re
 
+hertz = 2
 dataset_dir = 'C:/Users/aid338/Documents/forrest/'
+videos = [VideoStim(f) for f in glob(join(dataset_dir, 'stimuli/movie/fg_av_seg*'))]
+replace = False
 
-video = os.path.join(dataset_dir, 'stimuli/movie/fg_av_seg0.mkv')
-
-# Sample video
-conv = FrameSamplingConverter(hertz=1)
-derived = conv.transform(video)
-
+# Sampler and extractor
+conv = FrameSamplingConverter(hertz=hertz)
 ext = GoogleVisionAPIFaceExtractor(discovery_file='C:/Users/aid338/Documents/forrest-1c48f2c6a8c9.json')
 
-features = [ext.transform(frame) for frame in derived]
+for video in videos:
+    pattern = re.findall('seg([0-9]*)\.', video.filename)
+    if pattern:
+        segment = int(pattern[0])
+        out_file = (join('forrest_extract_results', 'clip{}_googleface_{}hz.csv').format(segment, hertz))
+        if  segment < 8 and (replace or (not isfile(out_file))):
+            derived = conv.transform(video)
+            features = [ext.transform(frame) for frame in derived]
+
+            feats_df = pd.concat([res.to_df() for res in features])
+            feats_df.to_csv(out_file)
